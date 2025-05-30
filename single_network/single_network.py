4324eb82be73fa5f3a4a11d7e4a61ed8685e746f
Keyneees <michele.nicoletti2000@gmail.com>
2025-05-30 10:20:45 +0200

    Added 100k buffer plots

--- single_network/single_network.py
+++ single_network/single_network.py
@@ -92,7 +92,7 @@ class Agent:
 
         self.q_network.to(self.device)
         self.optimizer=optim.Adam(self.q_network.parameters(), lr=lr)
-        self.replay_buffer=ReplayBuffer(10000)
+        self.replay_buffer=ReplayBuffer(100000)
 
     def select_action(self, state):
         if np.random.rand()<=self.epsilon:
